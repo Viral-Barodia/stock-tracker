4edafa6a97d2d26a62d4f84ffe34794bf76e29cf
Viral-Barodia <202001007@daiict.ac.in>
2024-08-19 19:41:30 +0530

    Menu component changes

--- src/Components/MenuComponent/MenuComponent.tsx
+++ src/Components/MenuComponent/MenuComponent.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './MenuComponent.scss';
 
 interface Props {
@@ -5,14 +6,22 @@ interface Props {
 }
 
 const MenuComponent = ({ headers }: Props) => {
+
+    const [activeMenu, setActiveMenu] = useState('Chart');
+    const changeMenuItem = async (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setActiveMenu(event.currentTarget.innerHTML);
+    }
     return (
         <section className="menu-bar">
             <ul className="menu-bar-list d-flex justify-content-between w-50">
                 {headers.map((header: string, index: number) =>
                     (<a className="menu-link" href="" key={index}>
                         <li
-                            className="menu-bar-item">{header}
+                            onClick={changeMenuItem}
+                            className={`menu-bar-item ${activeMenu==header ? 'active-menu' : ''}`}>{header}
                         </li>
+                        <div className={`menu-underline mt-2 ${activeMenu === header ? 'active-line' : ''}`}></div>
                     </a>))}
             </ul>
         </section>
