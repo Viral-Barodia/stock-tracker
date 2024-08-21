af6083e7d754e9267c815de868143bbb2cae3d44
Viral-Barodia <202001007@daiict.ac.in>
2024-08-21 12:09:14 +0530

    error handling and loading handled

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { useState, useEffect } from "react";
-import ChartComponent from "./Components/ChartComponent/ChartComponent"
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import ChartComponent from "./Components/ChartComponent/ChartComponent";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
 
   return (
     <div>
-      {loading ? (
-        <div>
-          <Skeleton height={50} width={200} />
-          <Skeleton height={400} />
-        </div>
-      ) : (
-        <ChartComponent />
-      )}
+      <ChartComponent />
     </div>
   )
 }
--- src/Components/ChartComponent/ChartComponent.tsx
+++ src/Components/ChartComponent/ChartComponent.tsx
@@ -12,6 +12,8 @@ import {
 import { useEffect, useState } from 'react';
 import { fetchData } from '../../services/fetchData.service';
 import { FireErrorToast } from '../../services/fireToastService';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 interface APIResponse {
     graphData: any,
@@ -28,21 +30,30 @@ const ChartComponent = () => {
     const [changeOverTime, setChangeOverTime] = useState<string>('');
     const [changePercentage, setChangePercentage] = useState<string>('');
     const [latestValue, setLatestValue] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
+
+    const fetchDataForRange = async (range: string) => {
+        setLoading(true);
+        setError(false);
+        try {
+            const data: APIResponse = await fetchData(range);
+            setGraphData(data.graphData);
+            setCurrency(data.currency);
+            setLatestValue(data.latestValue.toFixed(2));
+            setChangeOverTime(data.changeOverTime.toFixed(2));
+            setChangePercentage(data.changePercentage.toFixed(2));
+        } catch(e: any) {
+            setError(true);
+            FireErrorToast('');
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
-        fetchData(activeRange)
-            .then((data: APIResponse) => {
-                setGraphData(data.graphData);
-                setCurrency(data.currency);
-                setLatestValue(data.latestValue.toFixed(2));
-                setChangeOverTime(data.changeOverTime.toFixed(2));
-                setChangePercentage(data.changePercentage.toFixed(2));
-            })
-            .catch(() => {
-                FireErrorToast('');
-                return null;
-            })
-    }, [activeRange])
+        fetchDataForRange(activeRange);
+    }, [activeRange]);
 
     const changeRange = async (event: React.MouseEvent<HTMLElement>) => {
         setActiveRange(event.currentTarget.innerHTML);
@@ -62,63 +73,78 @@ const ChartComponent = () => {
         return null;
     };   
 
-    return ( graphData &&
+    return (
         <div className="chart-component-body m-auto w-75 d-flex flex-column mt-5">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <section className="header-price d-flex mb-5">
-                <div className="d-flex flex-column">
-                    <div className="current-price">{latestValue}</div>
-                    <div className={`stock-change ${Number(changeOverTime) >= 0 ? 'positive-change' : 'negative-change'}`}>{changeOverTime} ({changePercentage}%)</div>
+            { loading ? (
+                <div>
+                    <Skeleton height={50} width={200} />
+                    <Skeleton height={50} width={1122} />
+                    <Skeleton height={200} width={1122} />
                 </div>
-                <div className="currency mt-3 ms-2">{currency}</div>
-            </section>
-            <MenuComponent headers={headersList} />
-            <section className="chart-options align-items-center d-flex justify-content-between">
-            <div className="viewing-options d-flex w-25 justify-content-between">
-                <span className="viewing-options-items rounded p-2">
-                    <i className="fa fa-expand"></i>
-                    <span className="ms-2">Fullscreen</span>
-                </span>
-                <span className="viewing-options-items rounded p-2">
-                    <i className="fa fa-plus-circle"></i>
-                    <span className="ms-2">Compare</span>
-                </span>
-            </div>
-                <div className="interval-options d-flex justify-content-between">
-                    <ul className="interval-list m-0 d-flex">
-                        {rangeList.map((range: string, index: number) =>
-                            <li
-                                key={index}
-                                onClick={changeRange}
-                                className={`interval-item me-3 rounded-2 p-2 ${activeRange==range ? `active-interval` : ``}`}>{range}
-                            </li>)}
-                    </ul>
+            ) : error || !graphData ? (
+                <div className="text-center d-flex flex-column justify-content-center align-items-center h-100 w-100">
+                    <img src="/error.png" alt="Error image"></img>
+                    <p>Couldn't load the data! Maybe try again later?</p>
                 </div>
-            </section>
-            <section className="graph position-relative">
-                <AreaChart width={1122} height={200} data={graphData}>
-                    <defs>
-                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                        </linearGradient>
-                    </defs>
-                    <Area
-                        dataKey="closeValue"
-                        stroke="#4B40EE"
-                        fill="url(#colorUv)" />
-                    <CartesianGrid horizontal={false}/>
-                    <Tooltip content={<CustomTooltip />}/>
-                </AreaChart>
-                {latestValue !== null && (
-                    <div className="position-absolute rounded p-1 custom-tooltip">
-                        {latestValue.toLocaleString()}
+            ) :( 
+                <>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                    <section className="header-price d-flex mb-5">
+                        <div className="d-flex flex-column">
+                            <div className="current-price">{latestValue}</div>
+                            <div className={`stock-change ${Number(changeOverTime) >= 0 ? 'positive-change' : 'negative-change'}`}>{changeOverTime} ({changePercentage}%)</div>
+                        </div>
+                        <div className="currency mt-3 ms-2">{currency}</div>
+                    </section>
+                    <MenuComponent headers={headersList} />
+                    <section className="chart-options align-items-center d-flex justify-content-between">
+                    <div className="viewing-options d-flex w-25 justify-content-between">
+                        <span className="viewing-options-items rounded p-2">
+                            <i className="fa fa-expand"></i>
+                            <span className="ms-2">Fullscreen</span>
+                        </span>
+                        <span className="viewing-options-items rounded p-2">
+                            <i className="fa fa-plus-circle"></i>
+                            <span className="ms-2">Compare</span>
+                        </span>
                     </div>
-                )}
-                <BarChart width={1122} height={50} data={graphData} className="position-absolute">
-                    <Bar dataKey="volumeValue" fill="#6F7177" />
-                </BarChart>
-            </section>
+                        <div className="interval-options d-flex justify-content-between">
+                            <ul className="interval-list m-0 d-flex">
+                                {rangeList.map((range: string, index: number) =>
+                                    <li
+                                        key={index}
+                                        onClick={changeRange}
+                                        className={`interval-item me-3 rounded-2 p-2 ${activeRange==range ? `active-interval` : ``}`}>{range}
+                                    </li>)}
+                            </ul>
+                        </div>
+                    </section>
+                    <section className="graph position-relative">
+                        <AreaChart width={1122} height={200} data={graphData}>
+                            <defs>
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                                </linearGradient>
+                            </defs>
+                            <Area
+                                dataKey="closeValue"
+                                stroke="#4B40EE"
+                                fill="url(#colorUv)" />
+                            <CartesianGrid horizontal={false}/>
+                            <Tooltip content={<CustomTooltip />}/>
+                        </AreaChart>
+                        {latestValue !== null && (
+                            <div className="position-absolute rounded p-1 custom-tooltip">
+                                {latestValue.toLocaleString()}
+                            </div>
+                        )}
+                        <BarChart width={1122} height={50} data={graphData} className="position-absolute">
+                            <Bar dataKey="volumeValue" fill="#6F7177" />
+                        </BarChart>
+                    </section>
+                </>
+            )}
         </div>
     )
 }
